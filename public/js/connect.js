605cf03ba49d4ef88023f67a96ece079539a15b4
Ghsjulian <ghsjulian@gmail.com>
2024-09-16 18:28:26 +0600

    Two user connected

--- public/js/connect.js
+++ public/js/connect.js
@@ -74,18 +74,59 @@ socket.on("disconnect", () => {
     console.log("User Disconnected");
 });
 
+// SEND MESSAGES
+const sendMessage = message => {
+    socket.emit("sendMessage", message);
+};
+
+// UIPDATE BUTTON
+const updateButton = (type, id) => {
+    if (type === "REQUEST") {
+        const thisBtn = document.getElementById(id);
+        const conn = thisBtn.querySelector(".connect");
+        conn.onclick = () => {
+            sendMessage("ACCEPTED");
+        };
+        conn.style.backgroundColor = "#ff2f05";
+        conn.style.color = "#ffffff";
+        conn.style.border = "1px solid #ff2f05";
+        conn.textContent = "Accept Request";
+    } else if (type === "ACCEPTED") {
+        const thisBtn = document.getElementById(id);
+        const conn = thisBtn.querySelector(".connect");
+
+        // Add play Function here
+        conn.onclick = () => {
+            sendMessage("ACCEPTED");
+        };
+        conn.style.backgroundColor = "#07b929";
+        conn.style.color = "#ffffff";
+        conn.style.border = "1px solid #05af1a";
+        conn.textContent = "Play Now";
+    }
+};
 // CONNECTING WITH A USER
 const Connect = id => {
+    const thisBtn = document.getElementById(id);
+    const conn = thisBtn.querySelector(".connect");
+    // conn.disabled = true;
+    conn.style.backgroundColor = "#00700b";
+    conn.textContent = "Request Sent";
     socket.emit("connectToUser", id);
+    sendMessage("REQUEST");
 };
 
 // Handle new connection event
+
 socket.on("newConnection", userId => {
     console.log(`Connected to user ${userId}`);
-    socket.emit("sendMessage", "This is a test message...");
 });
 
 // Handle receive message event
-socket.on("receiveMessage", (message, userId) => {
-    console.log(`Received message from ${userId}: ${message}`);
+socket.on("receiveMessage", (message, u_id) => {
+    updateButton(message, u_id);
+    console.log(message);
+    console.log(u_id);
+    // alert(message.user.u_id);
+    // console.log(`Received message from ${userId}: ${message}`);
 });
